63e04cd64bcaefc675957ff117a5d6fbad4000d6
Paul Blanz <paul.blanz@gmail.com>
2017-12-22 11:42:50 -0500

    Added swagger ui support

--- src/main/java/com/blanzp/perftest/App.java
+++ src/main/java/com/blanzp/perftest/App.java
@@ -3,13 +3,29 @@ package com.blanzp.perftest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.spi.DocumentationType;
 
 @SpringBootApplication
 @EnableAsync
+@EnableSwagger2
 public class App {
 
     // http://localhost:8080/hello-world
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
     }
 }
